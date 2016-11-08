275c5da1ded6521f29efeb60bc9541ca509bf98b
Ole Andreas Hansen <oleaha@me.com>
2016-11-08 11:36:58 +0100

    Implement TextPair

--- src/main/java/fr/eurecom/dsg/mapreduce/Pairs/TextPair.java
+++ src/main/java/fr/eurecom/dsg/mapreduce/Pairs/TextPair.java
@@ -17,139 +17,147 @@ import org.apache.hadoop.io.WritableUtils;
  */
 public class TextPair implements WritableComparable<TextPair> {
 
-// TODO: add the pair objects as TextPair fields
-
-public void set(Text first, Text second) {
-  // TODO: implement the set method that changes the Pair content
-}
-
-public Text getFirst() {
-  // TODO: implement the first getter
-  return null;
-}
-
-public Text getSecond() {
-  // TODO: implement the second getter
-  return null;
-}
-  
-public TextPair() {
-  // TODO: implement the constructor, empty constructor MUST be implemented
-  //       for deserialization
-}
-
-public TextPair(String first, String second) {
-  this.set(new Text(first), new Text(second));
-}
-
-public TextPair(Text first, Text second) {
-  this.set(first, second);
-}
-
-@Override
-public void write(DataOutput out) throws IOException {
-  // TODO: write to out the serialized version of this such that
-  //       can be deserializated in future. This will be use to write to HDFS
-}
-
-@Override
-public void readFields(DataInput in) throws IOException {
-  // TODO: read from in the serialized version of a Pair and deserialize it
-}
-
-@Override
-public int hashCode() {
-  // TODO: implement hash
-  return 0;
-}
-
-@Override
-public boolean equals(Object o) {
-  // TODO: implement equals
-  return false;
-}
-
-@Override
-public int compareTo(TextPair tp) {
- // TODO: implement the comparison between this and tp
- return 0;
-}
-
-@Override
-public String toString() {
-  // TODO: implement toString for text output format
-  return super.toString();
-}
+    // TODO: add the pair objects as TextPair fields
+    public Text first;
+    public Text second;
 
 
+    public void set(Text first, Text second) {
+        this.first = first;
+        this.second = second;
+    }
 
+    public Text getFirst() {
+        return this.first;
+    }
 
+    public Text getSecond() {
+        return this.second;
+    }
+
+    public TextPair() {
+        // TODO: implement the constructor, empty constructor MUST be implemented
+        //       for deserialization
+    }
+
+    public TextPair(String first, String second) {
+        this.set(new Text(first), new Text(second));
+    }
+
+    public TextPair(Text first, Text second) {
+        this.set(first, second);
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+        // TODO: write to out the serialized version of this such that
+        //       can be deserializated in future. This will be use to write to HDFS
+        this.first.write(out);
+        this.second.write(out);
+    }
+
+    @Override
+    public void readFields(DataInput in) throws IOException {
+        // TODO: read from in the serialized version of a Pair and deserialize it
+        this.first = new Text();
+        this.first.readFields(in);
+
+        this.second = new Text();
+        this.second.readFields(in);
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO: implement hash
+        return first.hashCode() ^ second.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        // TODO: implement equals
+        if(!(o instanceof TextPair))
+            return false;
+        TextPair tp = (TextPair) o;
+        return tp.first.equals(this.first) && tp.second.equals(this.second);
+    }
+
+    @Override
+    public int compareTo(TextPair tp) {
+        // TODO: implement the comparison between this and tp
+        return first.compareTo(tp.first) == 0 ? first.compareTo(tp.first) : second.compareTo(tp.second);
+    }
+
+    @Override
+    public String toString() {
+        // TODO: implement toString for text output format
+        return "(" + this.first + ",  " + this.second + ")";
+    }
 
 // DO NOT TOUCH THE CODE BELOW
 
-/** Compare two pairs based on their values */
-public static class Comparator extends WritableComparator {
- 
-  /** Reference to standard Hadoop Text comparator */
-  private static final Text.Comparator TEXT_COMPARATOR = new Text.Comparator();
- 
-  public Comparator() {
-    super(TextPair.class);
-  }
+    /** Compare two pairs based on their values */
+    public static class Comparator extends WritableComparator {
 
-  @Override
-  public int compare(byte[] b1, int s1, int l1,
-                     byte[] b2, int s2, int l2) {
-   
-    try {
-      int firstL1 = WritableUtils.decodeVIntSize(b1[s1]) + readVInt(b1, s1);
-      int firstL2 = WritableUtils.decodeVIntSize(b2[s2]) + readVInt(b2, s2);
-      int cmp = TEXT_COMPARATOR.compare(b1, s1, firstL1, b2, s2, firstL2);
-      if (cmp != 0) {
-        return cmp;
-      }
-      return TEXT_COMPARATOR.compare(b1, s1 + firstL1, l1 - firstL1,
-                                     b2, s2 + firstL2, l2 - firstL2);
-    } catch (IOException e) {
-      throw new IllegalArgumentException(e);
+        /** Reference to standard Hadoop Text comparator */
+        private static final Text.Comparator TEXT_COMPARATOR = new Text.Comparator();
+
+        public Comparator() {
+            super(TextPair.class);
+        }
+
+        @Override
+        public int compare(byte[] b1, int s1, int l1,
+                           byte[] b2, int s2, int l2) {
+
+            try {
+                int firstL1 = WritableUtils.decodeVIntSize(b1[s1]) + readVInt(b1, s1);
+                int firstL2 = WritableUtils.decodeVIntSize(b2[s2]) + readVInt(b2, s2);
+                int cmp = TEXT_COMPARATOR.compare(b1, s1, firstL1, b2, s2, firstL2);
+                if (cmp != 0) {
+                    return cmp;
+                }
+                return TEXT_COMPARATOR.compare(b1, s1 + firstL1, l1 - firstL1,
+                        b2, s2 + firstL2, l2 - firstL2);
+            } catch (IOException e) {
+                throw new IllegalArgumentException(e);
+            }
+        }
     }
-  }
-}
 
-static {
- WritableComparator.define(TextPair.class, new Comparator());
-}
+    static {
+        WritableComparator.define(TextPair.class, new Comparator());
+    }
 
-/** Compare just the first element of the Pair */
-public static class FirstComparator extends WritableComparator {
- 
- private static final Text.Comparator TEXT_COMPARATOR = new Text.Comparator();
- 
- public FirstComparator() {
-   super(TextPair.class);
- }
+    /** Compare just the first element of the Pair */
+    public static class FirstComparator extends WritableComparator {
 
- @Override
- public int compare(byte[] b1, int s1, int l1,
-                    byte[] b2, int s2, int l2) {
-   
-   try {
-     int firstL1 = WritableUtils.decodeVIntSize(b1[s1]) + readVInt(b1, s1);
-     int firstL2 = WritableUtils.decodeVIntSize(b2[s2]) + readVInt(b2, s2);
-     return TEXT_COMPARATOR.compare(b1, s1, firstL1, b2, s2, firstL2);
-   } catch (IOException e) {
-     throw new IllegalArgumentException(e);
-   }
- }
- 
-@SuppressWarnings("unchecked")
-@Override
- public int compare(WritableComparable a, WritableComparable b) {
-   if (a instanceof TextPair && b instanceof TextPair) {
-     return ((TextPair) a).getFirst().compareTo(((TextPair) b).getFirst());
-   }
-   return super.compare(a, b);
- }
+        private static final Text.Comparator TEXT_COMPARATOR = new Text.Comparator();
 
-}
+        public FirstComparator() {
+            super(TextPair.class);
+        }
+
+        @Override
+        public int compare(byte[] b1, int s1, int l1,
+                           byte[] b2, int s2, int l2) {
+
+            try {
+                int firstL1 = WritableUtils.decodeVIntSize(b1[s1]) + readVInt(b1, s1);
+                int firstL2 = WritableUtils.decodeVIntSize(b2[s2]) + readVInt(b2, s2);
+                return TEXT_COMPARATOR.compare(b1, s1, firstL1, b2, s2, firstL2);
+            } catch (IOException e) {
+                throw new IllegalArgumentException(e);
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public int compare(WritableComparable a, WritableComparable b) {
+            if (a instanceof TextPair && b instanceof TextPair) {
+                return ((TextPair) a).getFirst().compareTo(((TextPair) b).getFirst());
+            }
+            return super.compare(a, b);
+        }
+
+    }
 }
